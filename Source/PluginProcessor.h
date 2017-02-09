8dbb1546ee9eeaff3cf448c93f3cd848a0123cef
kaitken07 <kaitken07@gmail.com>
2017-02-09 15:48:14 +0000

    Setup Editor

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -59,7 +59,8 @@ public:
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (StereoPannerAudioProcessor)
-};
+		void timerCallback();
+  };
 
 
 #endif  // PLUGINPROCESSOR_H_INCLUDED
